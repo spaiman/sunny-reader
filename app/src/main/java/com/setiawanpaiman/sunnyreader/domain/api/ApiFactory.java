7f7ece3fabbe38d69519eac351ff035f9cddd7a6
Setiawan Paiman <setiawan.paiman@gdplabs.id>
2016-04-24 12:31:21 +0700

    Add ApiFactory and HackerNewsApi

---
+++ app/src/main/java/com/setiawanpaiman/sunnyreader/domain/api/ApiFactory.java
@@ -0,0 +1,45 @@
+package com.setiawanpaiman.sunnyreader.domain.api;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiFactory {
+
+    private final Retrofit mRetrofit;
+
+    public ApiFactory(@NonNull String baseUrl, @NonNull Gson gson) {
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request request = chain.request();
+                        Log.d("Retrofit", request.url().toString());
+                        return chain.proceed(request);
+                    }
+                })
+                .build();
+
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(httpClient)
+                .build();
+    }
+
+    public <T> T create(Class<T> clazz) {
+        return mRetrofit.create(clazz);
+    }
+}
---
+++ app/src/main/java/com/setiawanpaiman/sunnyreader/domain/api/HackerNewsApi.java
@@ -0,0 +1,23 @@
+package com.setiawanpaiman.sunnyreader.domain.api;
+
+import com.setiawanpaiman.sunnyreader.data.model.Comment;
+import com.setiawanpaiman.sunnyreader.data.model.Story;
+
+import rx.Observable;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface HackerNewsApi {
+
+    @GET("v0/topstories.json")
+    Observable<List<Long>> getTopStories();
+
+    @GET("v0/item/{storyId}.json")
+    Observable<Story> getStory(@Path("storyId") long storyId);
+
+    @GET("v0/item/{commentId}.json")
+    Observable<Comment> getComment(@Path("commentId") long commentId);
+}
