f020c36a2cb1f78b662594e511f186f1d0778fd4
Setiawan Paiman <setiawan.paiman@gdplabs.id>
2016-04-24 17:04:56 +0700

    Limit ItemCache max items to 5000

--- app/src/main/java/com/setiawanpaiman/sunnyreader/data/db/ItemCache.java
+++ app/src/main/java/com/setiawanpaiman/sunnyreader/data/db/ItemCache.java
@@ -12,6 +12,8 @@ import com.raizlabs.android.dbflow.structure.BaseModel;
 @Table(database = AppDatabase.class, insertConflict = ConflictAction.REPLACE)
 public class ItemCache extends BaseModel {
 
+    private static final long MAX_ITEMS = 5000;
+
     @PrimaryKey(autoincrement = false)
     @Column
     long id;
@@ -31,16 +33,30 @@ public class ItemCache extends BaseModel {
         this.lastUpdate = System.currentTimeMillis();
     }
 
+    @Override
+    public void save() {
+        super.save();
+
+        long count = SQLite.selectCountOf().from(ItemCache.class).count();
+        if (count > MAX_ITEMS) {
+            SQLite.delete()
+                    .from(ItemCache.class)
+                    .where(ItemCache_Table.id.in(
+                            SQLite.select(ItemCache_Table.id)
+                                    .from(ItemCache.class)
+                                    .orderBy(ItemCache_Table.lastUpdate, true)
+                                    .limit((int) (count - MAX_ITEMS))
+                    ))
+                    .query();
+        }
+    }
+
     public long getId() {
         return id;
     }
 
     public String getResponse() {
         return response;
-    }
-
-    public long getLastUpdate() {
-        return lastUpdate;
     }
 
     @Nullable
