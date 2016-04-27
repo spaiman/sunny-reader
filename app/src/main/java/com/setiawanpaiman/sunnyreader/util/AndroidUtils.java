bd347d633053be506e75dffafdbd97f928c69b21
Setiawan Paiman <setiawan.paiman@gdplabs.id>
2016-04-27 23:42:17 +0700

    Add AndroidUtils

---
+++ app/src/main/java/com/setiawanpaiman/sunnyreader/util/AndroidUtils.java
@@ -0,0 +1,27 @@
+package com.setiawanpaiman.sunnyreader.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.support.v4.widget.SwipeRefreshLayout;
+
+public final class AndroidUtils {
+
+    private AndroidUtils() {
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        return ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE))
+                .getActiveNetworkInfo() != null;
+    }
+
+    public static void setSwipeRefreshing(final SwipeRefreshLayout swipeRefreshLayout,
+                                          final boolean isRefreshing) {
+        if(swipeRefreshLayout != null) {
+            swipeRefreshLayout.post(new Runnable() {
+                public void run() {
+                    swipeRefreshLayout.setRefreshing(isRefreshing);
+                }
+            });
+        }
+    }
+}
