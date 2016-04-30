14ef38df5b2556f988c196a28080865af144055b
Setiawan Paiman <setiawan.paiman@gdplabs.id>
2016-04-30 17:27:57 +0700

    Add setTitle method in BaseFragment

--- app/src/main/java/com/setiawanpaiman/sunnyreader/ui/fragment/BaseFragment.java
+++ app/src/main/java/com/setiawanpaiman/sunnyreader/ui/fragment/BaseFragment.java
@@ -1,11 +1,21 @@
 package com.setiawanpaiman.sunnyreader.ui.fragment;
 
+import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
 import com.setiawanpaiman.sunnyreader.HackerNewsApplication;
 import com.setiawanpaiman.sunnyreader.di.component.ApplicationComponent;
 
 public class BaseFragment extends Fragment {
+
+    public void setTitle(@StringRes int resId) {
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(resId);
+        }
+    }
 
     public final ApplicationComponent getApplicationComponent() {
         return ((HackerNewsApplication) getActivity().getApplication()).getApplicationComponent();
