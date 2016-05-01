76344e3e27bc5fcc0e3f6fa55b94249f806abb32
Setiawan Paiman <setiawan.paiman@gdplabs.id>
2016-05-01 13:30:31 +0700

    Add expand/collapse comments and show total replies

--- app/src/main/java/com/setiawanpaiman/sunnyreader/ui/adapter/CommentAdapter.java
+++ app/src/main/java/com/setiawanpaiman/sunnyreader/ui/adapter/CommentAdapter.java
@@ -7,6 +7,7 @@ import android.text.Html;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
 import android.text.method.LinkMovementMethod;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,8 @@ import com.setiawanpaiman.sunnyreader.data.model.Comment;
 import com.setiawanpaiman.sunnyreader.data.model.Story;
 import com.setiawanpaiman.sunnyreader.util.AndroidUtils;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -29,10 +32,14 @@ public class CommentAdapter extends EndlessListAdapter<Comment, RecyclerView.Vie
     private static final int MAX_DP_ELEVATION = 6;
 
     private Story mStory;
+    private List<Comment> mAllComments;
+    private SparseBooleanArray mExpanded;
 
     public CommentAdapter(Context context, Story story) {
         super(context);
         mStory = story;
+        mAllComments = new ArrayList<>();
+        mExpanded = new SparseBooleanArray();
     }
 
     @Override
@@ -43,7 +50,23 @@ public class CommentAdapter extends EndlessListAdapter<Comment, RecyclerView.Vie
             return new StoryDetailViewHolder(view);
         } else {
             final View view = inflater.inflate(R.layout.item_comment, parent, false);
-            return new ViewHolder(view);
+            final ViewHolder vh = new ViewHolder(view);
+            View.OnClickListener onClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = vh.getAdapterPosition();
+                    if (pos >= 0) {
+                        if (isExpanded(pos)) {
+                            collapseComments(pos);
+                        } else {
+                            expandComments(pos);
+                        }
+                    }
+                }
+            };
+            vh.contentContainer.setOnClickListener(onClickListener);
+            vh.txtContent.setOnClickListener(onClickListener);
+            return vh;
         }
     }
 
@@ -55,6 +78,14 @@ public class CommentAdapter extends EndlessListAdapter<Comment, RecyclerView.Vie
             Comment comment = mData.get(position - 1);
             ViewHolder vh = (ViewHolder) holder;
             vh.txtAuthor.setText(comment.getAuthor());
+            if (comment.getTotalReplies() > 0 && !isExpanded(position)) {
+                vh.txtTotalReplies.setText(mContext.getResources().getQuantityString(
+                        R.plurals.total_replies, comment.getTotalReplies(),
+                        comment.getTotalReplies()));
+                vh.txtTotalReplies.setVisibility(View.VISIBLE);
+            } else {
+                vh.txtTotalReplies.setVisibility(View.GONE);
+            }
             vh.txtTime.setText(DateUtils.getRelativeTimeSpanString(
                     TimeUnit.SECONDS.toMillis(comment.getTimestamp()), System.currentTimeMillis(),
                     DateUtils.SECOND_IN_MILLIS, DateUtils.FORMAT_SHOW_DATE));
@@ -74,10 +105,73 @@ public class CommentAdapter extends EndlessListAdapter<Comment, RecyclerView.Vie
         if (refresh) {
             clear();
         }
-        int lastSize = mData.size();
-        int newCount = results.size();
-        mData.addAll(results);
-        notifyItemRangeInserted(lastSize + 1, newCount);
+        int oldSize = mData.size();
+        mAllComments.addAll(results);
+        int addedCount = 0;
+        for (Comment newComment : results) {
+            if (newComment.getDepth() <= 0) {
+                mData.add(newComment);
+                addedCount++;
+            }
+        }
+        if (addedCount > 0) notifyItemRangeInserted(oldSize + 1, addedCount);
+    }
+
+    private boolean isExpanded(int adapterPos) {
+        Comment comment = mData.get(adapterPos - 1);
+        int allPos = mAllComments.indexOf(comment);
+        return mExpanded.get(allPos, false);
+    }
+
+    private void expandComments(int adapterPos) {
+        int actualPos = adapterPos - 1;
+        Comment parentComment = mData.get(actualPos);
+        int parentDepth = parentComment.getDepth();
+        int parentPos = mAllComments.indexOf(parentComment);
+        int addedCount = 0;
+        int maxDepth = parentDepth + 1;
+        for (int i = parentPos + 1, len = mAllComments.size(); i < len; i++) {
+            Comment childComment = mAllComments.get(i);
+            if (childComment.getDepth() < maxDepth) maxDepth--;
+
+            if (childComment.getDepth() <= parentDepth) break;
+            else if (childComment.getDepth() <= maxDepth) {
+                if (mExpanded.get(i, false)) maxDepth = childComment.getDepth() + 1;
+                mData.add(actualPos + addedCount + 1, childComment);
+                addedCount++;
+            }
+        }
+        mExpanded.put(parentPos, true);
+
+        notifyItemChanged(actualPos + 1);
+        notifyItemRangeInserted(actualPos + 2, addedCount);
+    }
+
+    private void collapseComments(int adapterPos) {
+        int actualPos = adapterPos - 1;
+        Comment parentComment = mData.get(actualPos);
+        int parentDepth = parentComment.getDepth();
+        int parentPos = mAllComments.indexOf(parentComment);
+        int removedCount = 0;
+        Iterator<Comment> iterator = mData.listIterator(actualPos + 1);
+        while (iterator.hasNext()) {
+            Comment childComment = iterator.next();
+            if (childComment.getDepth() <= parentDepth) break;
+
+            iterator.remove();
+            removedCount++;
+        }
+        mExpanded.put(parentPos, false);
+
+        notifyItemChanged(actualPos + 1);
+        notifyItemRangeRemoved(actualPos + 2, removedCount);
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+        mAllComments.clear();
+        mExpanded.clear();
     }
 
     @Override
@@ -130,8 +224,14 @@ public class CommentAdapter extends EndlessListAdapter<Comment, RecyclerView.Vie
         @BindView(R.id.card_view)
         CardView cardView;
 
+        @BindView(R.id.content_container)
+        View contentContainer;
+
         @BindView(R.id.txt_author)
         TextView txtAuthor;
+
+        @BindView(R.id.txt_total_replies)
+        TextView txtTotalReplies;
 
         @BindView(R.id.txt_time)
         TextView txtTime;
