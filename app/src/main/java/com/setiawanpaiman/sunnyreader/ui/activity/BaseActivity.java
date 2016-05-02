181e90a422b555ecbe29097042334b0550f7c140
Setiawan Paiman <setiawan.paiman@gdplabs.id>
2016-05-02 15:55:10 +0700

    Remove unused getApplicationComponent in BaseActivity

--- app/src/main/java/com/setiawanpaiman/sunnyreader/ui/activity/BaseActivity.java
+++ app/src/main/java/com/setiawanpaiman/sunnyreader/ui/activity/BaseActivity.java
@@ -2,12 +2,6 @@ package com.setiawanpaiman.sunnyreader.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 
-import com.setiawanpaiman.sunnyreader.HackerNewsApplication;
-import com.setiawanpaiman.sunnyreader.di.component.ApplicationComponent;
-
 public class BaseActivity extends AppCompatActivity {
 
-    public final ApplicationComponent getApplicationComponent() {
-        return ((HackerNewsApplication) getApplication()).getApplicationComponent();
-    }
 }
