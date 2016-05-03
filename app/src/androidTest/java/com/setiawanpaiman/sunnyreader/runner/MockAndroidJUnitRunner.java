833b5e4c38b98892fb6dc536b4c62429171157a9
Setiawan Paiman <setiawan.paiman@gdplabs.id>
2016-05-03 12:08:25 +0700

    Unlock and turn on screen before start instrumentation tests

--- app/src/androidTest/java/com/setiawanpaiman/sunnyreader/runner/MockAndroidJUnitRunner.java
+++ app/src/androidTest/java/com/setiawanpaiman/sunnyreader/runner/MockAndroidJUnitRunner.java
@@ -1,12 +1,38 @@
 package com.setiawanpaiman.sunnyreader.runner;
 
 import android.app.Application;
+import android.app.KeyguardManager;
 import android.content.Context;
+import android.os.PowerManager;
 import android.support.test.runner.AndroidJUnitRunner;
 
 import com.setiawanpaiman.sunnyreader.AndroidTestApplication;
 
 public class MockAndroidJUnitRunner extends AndroidJUnitRunner {
+
+    private PowerManager.WakeLock mWakeLock;
+
+    @Override
+    public void callApplicationOnCreate(Application app) {
+        // Unlock the screen
+        KeyguardManager keyguard = (KeyguardManager) app.getSystemService(Context.KEYGUARD_SERVICE);
+        keyguard.newKeyguardLock(getClass().getSimpleName()).disableKeyguard();
+
+        // Start a wake lock
+        PowerManager power = (PowerManager) app.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = power.newWakeLock(
+                PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP
+                        | PowerManager.ON_AFTER_RELEASE, getClass().getSimpleName());
+        mWakeLock.acquire();
+
+        super.callApplicationOnCreate(app);
+    }
+
+    @Override
+    public void onDestroy() {
+        mWakeLock.release();
+        super.onDestroy();
+    }
 
     @Override
     public Application newApplication(ClassLoader cl, String className, Context context)
